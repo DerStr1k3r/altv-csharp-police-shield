84fd8764e14543d9f45463336867190dc81aa9ca
DerStr1k3r <support@derstr1k3r.com>
2024-10-12 08:21:10 +0200

    First Version

---
+++ README.md
@@ -0,0 +1,48 @@
+# altv-csharp-police-shield
+
+![](preview.png)
+
+## Features
+
+This ressource adds a useable ballistic & riot shield. They
+- Block bullets
+- Can be used with a nightstick as sidearm
+
+## Usage
+
+To equip a ballistic shield emit the following **serverside**: 
+
+```alt.emit("Server:Shield:UseShield", client, true)```
+
+To equip a riot shield emit the following **serverside**:
+
+```alt.emit("Server:Shield:UseShield", client, false)```
+
+Or use the following code: **serverside**:
+
+```if (!player.HasData("shield")){
+    bool shieldstate = true;
+    int istate = shieldstate ? 1 : 0;
+    player.SetStreamSyncedMetaData("shield", istate);
+    player.SetStreamSyncedMetaData("shieldstatus", istate);
+} else {
+    player.DeleteStreamSyncedMetaData("shield");
+    player.DeleteStreamSyncedMetaData("shieldstatus");
+}```
+
+## Test setup
+
+If you just want to test the ressource add the following
+
+to the **index.js or whatever**:
+
+```
+import * as shield from '/your_framework_ui_folder/shield.js';
+```
+
+## Credits
+
+Client part from benstrobel 
+CSharp Server part from DerStr1k3r
+
+With that anyone can (un)equip the shield by pressing "L"
---
+++ server/serverside.cs
@@ -0,0 +1,55 @@
+using AltV.Net;
+using AltV.Net.Async;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DerStr1k3r.Handler
+{
+    class ShieldHandler : IScript
+    {
+        [AsyncClientEvent("Server:Shield:UseShield")]
+        public async Task UseItem(RoleplayPlayer player)
+        {
+            try
+            {
+				    if (!player.HasData("shield"))
+                    {
+						// Police Shield angelegt
+                        bool shieldstate = true;
+                        int istate = shieldstate ? 1 : 0;
+                        player.SetStreamSyncedMetaData("shield", istate);
+                        player.SetStreamSyncedMetaData("shieldstatus", istate);
+
+                    }
+                    else
+                    {
+						// Police Shield abgelegt
+                        player.DeleteStreamSyncedMetaData("shield");
+                        player.DeleteStreamSyncedMetaData("shieldstatus");
+                    }
+			}
+		}
+		
+        [AsyncClientEvent("playerEnteringVehicle")]
+        public async Task UseItem(RoleplayPlayer player)
+        {
+            try
+            {
+				    player.EmitLocked("Client:Shield:RemoveShieldForVehicle");
+			}
+		}
+
+        [AsyncClientEvent("playerLeftVehicle")]
+        public async Task UseItem(RoleplayPlayer player)
+        {
+            try
+            {
+				    player.EmitLocked("Client:Shield:AddShieldAfterVehicle");
+			}
+		}
+	}
+}		
